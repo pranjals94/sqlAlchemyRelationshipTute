7d5e6cabf7c34df2897f7dd92de67f7ac06f7c4c
pranjal <pranjals94@gmail.com>
2023-04-12 12:44:48 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,102 @@
+import database
+import model
+# from models import testModel
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI, Depends
+from database import engine
+from sqlalchemy.orm import Session
+from starlette.staticfiles import StaticFiles
+
+# if using virtual environment activate it and then type the following.
+# pip uninstall <packagename> # uninstall a package
+# pip list #lists oll the modules
+# pip freeze > requirements.txt  #cli to generate requirements.txt
+# pip install -r requirements.txt # install oll the package at one go
+
+print("----------main.py file serving-------------------------")
+model.Base.metadata.create_all(bind=engine)  # create database
+
+# app = FastAPI(docs_url="/documentation", redoc_url=None)  # Disable swagger (auto API UI)
+
+app = FastAPI()
+
+# --------------allow cors--------------------------
+origins = [
+    "http://localhost:3000",
+    "localhost:3000",
+]
+# A "middleware" is a function that works with every request before it is processed by any specific path operation.
+# And also with every response before returning it. refer docs for more info
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+# -- APIs'----
+@app.get("/A")
+async def read_item(db: Session = Depends(database.get_db)):
+    # one to many
+
+    db_child = model.A_Child(childName="Chintu A")
+    db_child_1 = model.A_Child(childName="Rickey A")
+    db_parent = model.A_Parent(parentName="Bob A", children=[db_child, db_child_1])  # children should be list-like
+    db.add(db_parent)
+    db.commit()
+    db.refresh(db_parent)
+    return {"item_id": db_parent.children}
+
+
+@app.get("/B")
+async def read_item(db: Session = Depends(database.get_db)):
+    # To establish a bidirectional relationship in one-to-many, where the “reverse” side is a many to one
+
+    # db_child = model.B_Child(childName="Chintu B")
+    # db_child_1 = model.B_Child(childName="Rickey B")
+    # db_parent = model.B_Parent(parentName="Bob B", children=[db_child, db_child_1])  # children should be list-like
+    # db.add(db_parent)
+    # db.commit()
+
+    # db_parent = model.B_Parent(parentName="Tom B")
+    # db_child = model.B_Child(childName="Layla B", parent=db_parent)
+    # db.add(db_child)
+    # db.commit()
+
+    # parent = db.get(model.B_Parent, 1) #  we can either use db.get(model.B_Parent or model.B_Child) using both
+    # together gives error.
+    child = db.get(model.B_Child, 1)
+    print(child.parent)  # if we don't use this statement here , only parent_id is sent in the return statement
+
+    return {"child": child}
+
+
+@app.get("/D")
+async def read_item(db: Session = Depends(database.get_db)):
+    db_child = model.D_Child(childName="Chintu D")
+    db_child_1 = model.D_Child(childName="Rickey D")
+    db_parent = model.D_Parent(parentName="Bob D", children=[db_child, db_child_1])  # children should be list-like
+    db.add(db_parent)
+    db.commit()
+    db.refresh(db_parent)
+    return {"item_id": db_parent.children}
+
+
+@app.get("/E")
+async def read_item(db: Session = Depends(database.get_db)):
+    db_child = model.E_Child(childName="Chintu E")
+    db_parent = model.E_Parent(parentName="Bob E", children=db_child)  # children cannot be list like refer model E
+    db.add(db_parent)
+    db.commit()
+
+    # try to update the child
+    # foreign key id cannot be null error
+    # db_child_1 = model.E_Child(childName="Rickey E")
+    # db_parent = db.get(model.E_Parent, 1)
+    # db_parent.children = db_child_1
+    # db.add(db_parent)
+    # db.commit()
+    # db.refresh(db_parent)
+    return {"item_id": db_parent.children}
---
+++ model.py
@@ -0,0 +1,93 @@
+import datetime
+from sqlalchemy import Boolean, Column, ForeignKey, String, DateTime, Date, Time, Integer, Integer
+from sqlalchemy.orm import relationship
+from database import Base
+
+
+# -------------------one to many- parent to child, many to one- child to parent--------------
+class A_Parent(Base):
+    __tablename__ = "A_parent_table"  # table names are converted to lower case in the database
+    id = Column(Integer, primary_key=True)
+    parentName = Column(String(50))
+    children = relationship(
+        "A_Child")  # children should be list-like, also use backref paremeter to explicitely define one directional relationship, mentioned in C table below
+
+
+class A_Child(Base):
+    __tablename__ = "A_child_table"
+    id = Column(Integer, primary_key=True)
+    childName = Column(String(50))
+    parent_id = Column(Integer, ForeignKey("A_parent_table.id"))
+
+
+# ---To establish a bidirectional relationship in one-to-many, where the “reverse” side is a many to one---
+#  using back_populates relationship parameter
+class B_Parent(Base):
+    __tablename__ = "B_parent_table"
+    id = Column(Integer, primary_key=True)
+    parentName = Column(String(50))
+    children = relationship("B_Child", back_populates="parent")  # children should be list-like
+
+
+class B_Child(Base):
+    __tablename__ = "B_child_table"
+    id = Column(Integer, primary_key=True)
+    childName = Column(String(50))
+    parent_id = Column(Integer, ForeignKey("B_parent_table.id"))
+    parent = relationship("B_Parent", back_populates="children")
+
+
+# Alternatively, the relationship.backref option may be used on a single relationship() instead of using
+# relationship.back_populates on the Parent table.
+class C_Parent(Base):
+    __tablename__ = "C_parent_table"
+    id = Column(Integer, primary_key=True)
+    parentName = Column(String(50))
+    children = relationship("C_Child",
+                            backref="parent")  # it will create the backref="parent" relation in the child tabe and use by itself we don't need to border
+
+
+class C_Child(Base):
+    __tablename__ = "C_child_table"
+    id = Column(Integer, primary_key=True)
+    childName = Column(String(50))
+    parent_id = Column(Integer, ForeignKey("C_parent_table.id"))
+    # parent = relationship("C_Parent", back_populates="children") # cant use this statement when using backref in parent
+
+
+# Using foreign key ON DELETE c ascade
+class D_Parent(Base):
+    __tablename__ = "D_parent_table"
+    id = Column(Integer, primary_key=True)
+    parentName = Column(String(50))
+    children = relationship("D_Child", backref="parent", cascade="all, delete",
+                            passive_deletes=True, )  # delete all its child on deletion
+
+
+class D_Child(Base):
+    __tablename__ = "D_child_table"
+    id = Column(Integer, primary_key=True)
+    childName = Column(String(50))
+    parent_id = Column(Integer, ForeignKey("D_parent_table.id",
+                                           ondelete="CASCADE"))  # onDelete parameter will allow to delete from navicat despite of foreign keys
+
+
+# ---one-to-one-----
+# “collection” side ie the child is converted into a scalar relationship using the uselist=False
+# flag. when we load a Parent object, the Parent.child attribute will refer to a single Child object rather than a
+# collection. If we replace the value of Parent.child with a new Child object, the ORM’s unit of work process will
+# replace the previous Child row with the new one, setting the previous child.parent_id column to NULL by default
+# unless there are specific cascade behaviors set up.
+class E_Parent(Base):
+    __tablename__ = "E_parent_table"
+    id = Column(Integer, primary_key=True)
+    parentName = Column(String(50))
+    children = relationship("E_Child", back_populates="parent", uselist=False)
+
+
+class E_Child(Base):
+    __tablename__ = "E_child_table"
+    id = Column(Integer, primary_key=True)
+    childName = Column(String(50))
+    parent_id = Column(Integer, ForeignKey("E_parent_table.id"), nullable=False)
+    parent = relationship("E_Parent", back_populates="children")
